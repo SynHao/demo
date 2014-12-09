747188d02c41f68bb5e962d9412d04eb5a57146e
synhao <haoshuaiyu@126.com>
2014-12-09 10:17:46 +0800

    设置账户登录UI

---
+++ SynHaoData/RibbonData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace SynHaoData
+{
+    public partial class RibbonData
+    {
+        private void RibbonData_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+    }
+}
